839f142df145dbfa6b2b63768abde5ab0b3812d8
Neil Johnson <mr.neil.j@gmail.com>
2017-02-01 00:59:44 -0800

    Added first class

--- main.cpp
+++ main.cpp
@@ -2,3 +2,18 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+// Circle class declaration
+class Circle
+{
+private:
+    double radius;
+
+public:             // Member function prototypes
+    Circle();
+    void setRadius(double);
+    double calcArea();
+};
+
+// Circle member function implementation section
+
